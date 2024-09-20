c9133e212c4b08f0a25a03fce163dadbbf3e73cb
Nhan Chau <nhan.chau@tuni.fi>
2024-09-20 16:11:54 +0300

    chore: hanlding checkpointing

--- api_to_postgres/utils.py
+++ api_to_postgres/utils.py
@@ -3,9 +3,10 @@ import os
 from datetime import datetime, timedelta
 
 import requests
+from Io import read_checkpoint
 
 
-def get_airport_data(from_date: datetime, to_date: datetime, icao: str) -> dict:
+def fetch_airport_data(from_date: datetime, to_date: datetime, icao: str) -> dict:
     try:
         if (to_date - from_date) > timedelta(hours=12):
             raise ValueError(
@@ -44,3 +45,22 @@ def get_airport_data(from_date: datetime, to_date: datetime, icao: str) -> dict:
     except Exception as e:
         logging.error(f"Error fetching airport data: {e}")
         raise
+
+
+def split_into_12_hour_segments_with_checkpoint(
+    start_date: datetime, end_date: datetime
+) -> list:
+    if start_date >= end_date:
+        raise ValueError("start_date must be earlier than end_date")
+
+    (checkpoint_start, _) = read_checkpoint()
+    segments = []
+    current_start = start_date
+
+    while current_start < end_date:
+        current_end = min(current_start + timedelta(hours=12), end_date)
+        if current_start > checkpoint_start:
+            segments.append((current_start, current_end))
+        current_start = current_end
+
+    return segments
