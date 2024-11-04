9a43c66d5d62a7c9cd8534d0130ab41d2ea43cec
Nhan Chau <116027999+NhanChau2409@users.noreply.github.com>
2024-11-04 17:33:31 +0200

    chore: fix (#6)
    Co-authored-by: pro ject <dbtsnowflakeairbyte@gmail.com>

--- dbt/models/departures.sql
+++ dbt/models/departures.sql
@@ -1,12 +1,12 @@
 select
     f.value:number::string as flightnumber,
-    to_date(f.value:movement.revisedtime.utc)::string as flightdate,
+    to_date(f.value:movement.revisedTime.utc)::string as flightdate,
 
-    f.value:callsign::string as callsign,
-    f.value:codesharestatus::string as codesharestatus,
-    f.value:iscargo::boolean as iscargo,
+    f.value:callSign::string as callsign,
+    f.value:codeshareStatus::string as codesharestatus,
+    f.value:isCargo::boolean as iscargo,
     -- f.value:status::STRING AS flightStatus,
-    f.value:aircraft.modes::string as aircraft_modes,
+    -- f.value:aircraft.modes::string as aircraft_modes,
     f.value:aircraft.model::string as aircraft_model,
     f.value:aircraft.reg::string as aircraft_reg,
 
@@ -17,16 +17,16 @@ select
     f.value:movement.airport.iata::string as to_airport_iata,
     f.value:movement.airport.icao::string as to_airport_icao,
     f.value:movement.airport.name::string as to_airport_name,
-    f.value:movement.airport.timezone::string as to_airport_tz,
+    f.value:movement.airport.timeZone::string as to_airport_tz,
 
-    f.value:movement.baggagebelt::string as baggagebelt,
+    -- f.value:movement.baggageBelt::string as baggagebelt,
     f.value:movement.gate::string as gate,
     -- f.value:movement.quality::ARRAY AS quality,
     f.value:movement.terminal::string as terminal,
 
     -- f.value:movement.revisedTime.local::TIMESTAMP AS revisedTime_local,
-    f.value:movement.revisedtime.utc::string as revisedtime_utc,
+    f.value:movement.revisedTime.utc::string as revisedtime_utc,
 
     -- f.value:movement.scheduledTime.local::TIMESTAMP AS scheduledTime_local,
-    f.value:movement.scheduledtime.utc::string as scheduledtime_utc
-from {{ source("S3", "S3") }} s, lateral flatten(input => s.response, path => 'departures') f
+    f.value:movement.scheduledTime.utc::string as scheduledtime_utc
+from s3 s, lateral flatten(input => s.response, path => 'departures') f
